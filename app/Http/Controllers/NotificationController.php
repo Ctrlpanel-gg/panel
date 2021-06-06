02a2449350feeac6ab6ec0deb82f4fa140025af7
Ramon Robben <ramon.u.robben@gmail.com>
2021-06-06 20:17:52 +0200

    Cleaned up controllers with for PHP 8

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,12 +11,8 @@ use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Factory|View
-     */
-    public function index()
+    /** Display a listing of the resource. */
+    public function index(): View|Factory
     {
         $notifications = Auth::user()->notifications()->paginate();
 
@@ -25,34 +21,8 @@ class NotificationController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  string  $id
-     * @return Factory|View
-     */
-    public function show($id)
+    /** Display the specified resource. */
+    public function show(string $id): View|Factory
     {
         $notification = Auth::user()->notifications()->findOrFail($id);
 
@@ -60,39 +30,5 @@ class NotificationController extends Controller
         return view('notifications.show')->with([
             'notification' => $notification
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
