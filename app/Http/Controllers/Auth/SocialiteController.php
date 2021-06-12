178d672ed959fa9d28a6a1af835c17d0c63d2bc9
AVMG20 <rijstenpap1234@hotmail.com>
2021-06-12 14:50:51 +0200

    Discord 0Auth more optional, .env.example cleanup

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -13,8 +13,10 @@ class SocialiteController extends Controller
 {
     public function redirect()
     {
+        $scopes = !empty(env('DISCORD_BOT_TOKEN')) && !empty(env('DISCORD_GUILD_ID')) ? ['guilds.join'] : [];
+
         return Socialite::driver('discord')
-            ->scopes(['guilds.join'])
+            ->scopes($scopes)
             ->redirect();
     }
 
@@ -27,8 +29,8 @@ class SocialiteController extends Controller
         $discord = Socialite::driver('discord')->user();
         $discordUser = DiscordUser::find($discord->id);
 
-        $guildId = env('DISCORD_GUILD_ID', null);
-        $botToken = env('DISCORD_BOT_TOKEN', null);
+        $guildId = env('DISCORD_GUILD_ID');
+        $botToken = env('DISCORD_BOT_TOKEN');
 
         //force user into discord server
         //TODO Add event on failure, to notify ppl involved
@@ -47,8 +49,8 @@ class SocialiteController extends Controller
             //create discord user in db
             DiscordUser::create(array_merge($discord->user, ['user_id' => Auth::user()->id]));
             //update user
-            Auth::user()->increment('credits' , Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_DISCORD'));
-            Auth::user()->increment('server_limit' , Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD'));
+            Auth::user()->increment('credits', Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_DISCORD'));
+            Auth::user()->increment('server_limit', Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD'));
             Auth::user()->update(['discord_verified_at' => now()]);
         } else {
             $discordUser->update($discord->user);
