823cde9cf9b70282759c33440d2d54a057ad3673
AVMG20 <rijstenpap1234@hotmail.com>
2021-06-27 15:53:49 +0200

    Added REGISTER_IP_CHECK config option
    Prevent users from making multiple accounts using the same IP address

--- database/seeders/Seeds/ConfigurationSeeder.php
+++ database/seeders/Seeds/ConfigurationSeeder.php
@@ -18,16 +18,16 @@ class ConfigurationSeeder extends Seeder
         Configuration::firstOrCreate([
             'key' => 'INITIAL_CREDITS',
         ], [
-            'value' => '250',
-            'type'  => 'integer',
+            'value'       => '250',
+            'type'        => 'integer',
             'description' => 'The initial amount of credits the user starts with.'
         ]);
 
         Configuration::firstOrCreate([
             'key' => 'INITIAL_SERVER_LIMIT',
         ], [
-            'value' => '1',
-            'type'  => 'integer',
+            'value'       => '1',
+            'type'        => 'integer',
             'description' => 'The initial server limit the user starts with.'
         ]);
 
@@ -35,33 +35,33 @@ class ConfigurationSeeder extends Seeder
         Configuration::firstOrCreate([
             'key' => 'CREDITS_REWARD_AFTER_VERIFY_EMAIL',
         ], [
-            'value' => '250',
-            'type'  => 'integer',
+            'value'       => '250',
+            'type'        => 'integer',
             'description' => 'Increase in credits after the user has verified their email account.'
         ]);
 
         Configuration::firstOrCreate([
             'key' => 'SERVER_LIMIT_REWARD_AFTER_VERIFY_EMAIL',
         ], [
-            'value' => '2',
-            'type'  => 'integer',
+            'value'       => '2',
+            'type'        => 'integer',
             'description' => 'Increase in server limit after the user has verified their email account.'
         ]);
 
         //verify discord event
         Configuration::firstOrCreate([
-            'key'   => 'CREDITS_REWARD_AFTER_VERIFY_DISCORD',
-        ] , [
-            'value' => '375',
-            'type'  => 'integer',
+            'key' => 'CREDITS_REWARD_AFTER_VERIFY_DISCORD',
+        ], [
+            'value'       => '375',
+            'type'        => 'integer',
             'description' => 'Increase in credits after the user has verified their discord account.'
         ]);
 
         Configuration::firstOrCreate([
             'key' => 'SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD',
         ], [
-            'value' => '2',
-            'type'  => 'integer',
+            'value'       => '2',
+            'type'        => 'integer',
             'description' => 'Increase in server limit after the user has verified their discord account.'
         ]);
 
@@ -69,8 +69,8 @@ class ConfigurationSeeder extends Seeder
         Configuration::firstOrCreate([
             'key' => 'MINIMUM_REQUIRED_CREDITS_TO_MAKE_SERVER',
         ], [
-            'value' => '50',
-            'type'  => 'integer',
+            'value'       => '50',
+            'type'        => 'integer',
             'description' => 'The minimum amount of credits the user would need to make a server.'
         ]);
 
@@ -78,25 +78,36 @@ class ConfigurationSeeder extends Seeder
         Configuration::firstOrCreate([
             'key' => 'SERVER_LIMIT_AFTER_IRL_PURCHASE',
         ], [
-            'value' => '10',
-            'type'  => 'integer',
+            'value'       => '10',
+            'type'        => 'integer',
             'description' => 'updates the users server limit to this amount (unless the user already has a higher server limit) after making a purchase with real money, set to 0 to ignore this.',
         ]);
 
+
+        //force email and discord verification
         Configuration::firstOrCreate([
-            'key'   => 'FORCE_EMAIL_VERIFICATION',
-        ] , [
-            'value' => 'false',
-            'type'  => 'boolean',
+            'key' => 'FORCE_EMAIL_VERIFICATION',
+        ], [
+            'value'       => 'false',
+            'type'        => 'boolean',
             'description' => 'Force an user to verify the email adress before creating a server / buying credits.'
         ]);
 
         Configuration::firstOrCreate([
-            'key'   => 'FORCE_DISCORD_VERIFICATION',
-        ] , [
-            'value' => 'false',
-            'type'  => 'boolean',
+            'key' => 'FORCE_DISCORD_VERIFICATION',
+        ], [
+            'value'       => 'false',
+            'type'        => 'boolean',
             'description' => 'Force an user to link an Discord Account before creating a server / buying credits.'
+        ]);
+
+        //disable ip check on register
+        Configuration::firstOrCreate([
+            'key' => 'REGISTER_IP_CHECK',
+        ], [
+            'value'       => 'true',
+            'type'        => 'boolean',
+            'description' => 'Prevent users from making multiple accounts using the same IP address'
         ]);
     }
 }
