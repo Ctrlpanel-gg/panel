6a5f1a15cb08722100d57b37062dd6ba627ab378
Chirag <chiraglamba350@gmail.com>
2021-09-16 16:14:50 +0530

    lul

--- config/currency_codes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    "AUD", "BRL", "CAD", "CNY", "CZK", "DKK", "EUR", "HKD", "HUF", "ILS", "JPY", "MYR", "MXN", "TWD", "NZD", "NOK", "PHP", "PLN", "GBP", "RUB", "SGD", "SEK", "CHF", "THB", "USD",
-];
