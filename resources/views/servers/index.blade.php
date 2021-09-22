e3e2a6593b5c2cd260f6b4ee0dc4cfe222502892
Dennis <ownerdennis8@gmail.com>
2021-09-22 18:22:05 +0200

    Using {{CREDITS_DISPLAY_NAME}}

--- resources/views/servers/index.blade.php
+++ resources/views/servers/index.blade.php
@@ -85,11 +85,11 @@
                                     </tr>
                                     <tr>
                                         <td>Price per Hour</td>
-                                        <td>{{number_format($server->product->getHourlyPrice(),2,".")}} Credits</td>
+                                        <td>{{number_format($server->product->getHourlyPrice(),2,".")}} {{CREDITS_DISPLAY_NAME}}</td>
                                     </tr>
                                     <tr>
                                         <td>Price per Month</td>
-                                        <td>{{$server->product->getHourlyPrice()*24*30}} Credits</td>
+                                        <td>{{$server->product->getHourlyPrice()*24*30}} {{CREDITS_DISPLAY_NAME}}</td>
                                     </tr>
                                 </table>
                             </div>
