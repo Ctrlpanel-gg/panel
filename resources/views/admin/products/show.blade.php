28af0c053009682e2431deaf118f69eee0391bcb
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2021-10-01 23:21:49 +0200

    Add minimal credits per product

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -10,10 +10,10 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('admin.users.index')}}">Products</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">Products</a></li>
                         <li class="breadcrumb-item"><a class="text-muted"
-                                                       href="{{route('admin.products.show' , $product->id)}}">Show</a>
+                                href="{{ route('admin.products.show', $product->id) }}">Show</a>
                         </li>
                     </ol>
                 </div>
@@ -30,11 +30,15 @@
                 <div class="card-header d-flex justify-content-between">
                     <h5 class="card-title"><i class="fas fa-sliders-h mr-2"></i>Product</h5>
                     <div class="ml-auto">
-                        <a data-content="Edit" data-trigger="hover" data-toggle="tooltip" href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-sm btn-info mr-1"><i class="fas fa-pen"></i></a>
-                        <form class="d-inline" onsubmit="return submitResult();" method="post" action="{{ route('admin.products.destroy', $product->id) }}">
+                        <a data-content="Edit" data-trigger="hover" data-toggle="tooltip"
+                            href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-sm btn-info mr-1"><i
+                                class="fas fa-pen"></i></a>
+                        <form class="d-inline" onsubmit="return submitResult();" method="post"
+                            action="{{ route('admin.products.destroy', $product->id) }}">
                             {{ csrf_field() }}
-                            {{ method_field("DELETE") }}
-                            <button data-content="Delete" data-trigger="hover" data-toggle="tooltip" class="btn btn-sm btn-danger mr-1"><i class="fas fa-trash"></i></button>
+                            {{ method_field('DELETE') }}
+                            <button data-content="Delete" data-trigger="hover" data-toggle="tooltip"
+                                class="btn btn-sm btn-danger mr-1"><i class="fas fa-trash"></i></button>
                         </form>
                     </div>
                 </div>
@@ -47,9 +51,9 @@
                                     <label>ID</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->id}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->id }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -60,9 +64,9 @@
                                     <label>Name</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->name}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->name }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -73,9 +77,26 @@
                                     <label>Price</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           <i class="fas fa-coins mr-1"></i>{{$product->price}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        <i class="fas fa-coins mr-1"></i>{{ $product->price }}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <label>Minimum {{ CREDITS_DISPLAY_NAME }}</label>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        @if ($product->minimum_credits == -1)
+                                            <i class="fas fa-coins mr-1"></i>{{ $minimum_credits }}
+                                        @else
+                                            <i class="fas fa-coins mr-1"></i>{{ $product->minimum_credits }}
+                                        @endif
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -87,9 +108,9 @@
                                     <label>Memory</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->memory}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->memory }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -100,9 +121,9 @@
                                     <label>CPU</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->cpu}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->cpu }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -113,9 +134,9 @@
                                     <label>Swap</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->swap}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->swap }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -126,9 +147,9 @@
                                     <label>Disk</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->disk}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->disk }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -139,9 +160,9 @@
                                     <label>IO</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->io}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->io }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -152,9 +173,9 @@
                                     <label>Databases</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->databases}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->databases }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -165,9 +186,9 @@
                                     <label>Allocations</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->allocations}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->allocations }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -178,9 +199,9 @@
                                     <label>Created At</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->created_at ? $product->created_at->diffForHumans() : ''}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->created_at ? $product->created_at->diffForHumans() : '' }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -192,9 +213,9 @@
                                     <label>Description</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span class="d-inline-block text-truncate">
-                                           {{$product->description}}
-                                       </span>
+                                    <span class="d-inline-block text-truncate">
+                                        {{ $product->description }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -206,9 +227,9 @@
                                     <label>Updated At</label>
                                 </div>
                                 <div class="col-lg-8">
-                                       <span style="max-width: 250px;" class="d-inline-block text-truncate">
-                                           {{$product->updated_at ? $product->updated_at->diffForHumans() : ''}}
-                                       </span>
+                                    <span style="max-width: 250px;" class="d-inline-block text-truncate">
+                                        {{ $product->updated_at ? $product->updated_at->diffForHumans() : '' }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
