0516ba360ac084375ff67e2076099ba03c06fea0
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-10-04 14:41:50 +0200

    Create validation.php

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted' => 'Het :attribute moet worden geaccepteerd.',
+    'active_url' => 'Het :attribute is geen geldige URL.',
+    'after' => 'Het :attribute moet een datum zijn na :date.',
+    'after_or_equal' => 'Het :attribute moet een datum zijn na of gelijk zijn aan :date.',
+    'alpha' => 'Het :attribute mag alleen letters bevatten.',
+    'alpha_dash' => 'Het :attribute mag alleen letters, cijfers, streepjes en underscores bevatten.',
+    'alpha_num' => 'Het :attribute mag alleen letters en cijfers bevatten.',
+    'array' => 'Het :attribute moet een array zijn.',
+    'before' => 'Het :attribute moet een datum eerder zijn dan :date.',
+    'before_or_equal' => 'Het :attribute moet een datum vóór of gelijk zijn aan :date.',
+    'between' => [
+        'numeric' => 'Het :attribute moet tussen  :min en :max zijn.',
+      'file' => 'Het :attribute moet tussen :min en :max kilobytes zijn.',
+        'string' => 'Het :attribute moet tussen de :min en :max karakters zijn.',
+        'array' => 'Het :attribute moet tussen :min en :max items bevatten.',
+    ],
+    'boolean' => 'Het :attribute veld moet waar of fout zijn.',
+    'confirmed' => 'Het :attribute bevestiging komt niet overeen.',
+    'date' => 'Het :attribute is geen geldige datum.',
+    'date_equals' => 'Het :attribute moet een datum zijn die gelijk is aan :date.',
+    'date_format' => 'Het :attribute komt niet overeen met het formaat :format.',
+    'different' => 'The :attribute and :other must be different.',
+    'digits' => 'The :attribute must be :digits digits.',
+    'digits_between' => 'The :attribute must be between :min and :max digits.',
+    'dimensions' => 'The :attribute has invalid image dimensions.',
+    'distinct' => 'The :attribute field has a duplicate value.',
+    'email' => 'The :attribute must be a valid email address.',
+    'ends_with' => 'The :attribute must end with one of the following: :values.',
+    'exists' => 'The selected :attribute is invalid.',
+    'file' => 'The :attribute must be a file.',
+    'filled' => 'The :attribute field must have a value.',
+    'gt' => [
+        'numeric' => 'The :attribute must be greater than :value.',
+        'file' => 'The :attribute must be greater than :value kilobytes.',
+        'string' => 'The :attribute must be greater than :value characters.',
+        'array' => 'The :attribute must have more than :value items.',
+    ],
+    'gte' => [
