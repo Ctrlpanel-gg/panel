c92b435f659b14622991efd6e9e64547b6484fc7
WBLKLeipe <ownerdennis8@gmail.com>
2021-10-26 16:54:51 +0200

    Comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,11 +27,13 @@ class HomeController extends Controller
         foreach (Auth::user()->servers as $server){
             $usage += $server->product->price;
         }
+
+        // START OF THE TIME-REMAINING-BOX
         if(Auth::user()->Credits() > 0.01 and $usage > 0){
             $days = number_format((Auth::user()->Credits()*30)/$usage,2,'.','');
             $hours = number_format(Auth::user()->Credits()/($usage/30/24),2,'.','');
 
-
+        // DEFINE THE BACKGROUND COLOR
             if($days >= 15){
                 $bg =  "success";
             }elseif ($days >= 8 && $days <= 14){
@@ -39,7 +41,7 @@ class HomeController extends Controller
             }elseif ($days <= 7){  
                $bg =  "danger";      
         }
-
+            // DEFINE WETHER DAYS OR HOURS REMAIN
             if($days < "1"){
                 if($hours < "1"){
                     $boxText = 'You ran out of Credits ';
@@ -54,7 +56,7 @@ class HomeController extends Controller
                 }
         }
         
-    
+    // RETURN ALL VALUES
         return view('home')->with([
             'useage' => $usage,
             'useful_links' => UsefulLink::all()->sortBy('id'),
