20f9ddeae2367eb2b3ab4883f889eea5207bf5ab
AVMG20 <rijstenpap1234@hotmail.com>
2021-11-11 22:05:42 +0100

    Fixed a bug where products where only fetched using node and not egg,node

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::middleware(['auth', 'checkSuspended'])->group(function () {
     #routes made for server create page to fetch product info
     Route::get('/products/nodes/egg/{egg?}', [FrontProductController::class, 'getNodesBasedOnEgg'])->name('products.nodes.egg');
     Route::get('/products/locations/egg/{egg?}', [FrontProductController::class, 'getLocationsBasedOnEgg'])->name('products.locations.egg');
-    Route::get('/products/products/node/{node?}', [FrontProductController::class, 'getProductsBasedOnNode'])->name('products.products.node');
+    Route::get('/products/products/{egg?}/{node?}', [FrontProductController::class, 'getProductsBasedOnNode'])->name('products.products.node');
 
     #payments
     Route::get('checkout/{paypalProduct}', [PaymentController::class, 'checkOut'])->name('checkout');
