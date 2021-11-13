eeddec528e0d17df85d9fd087856480d991407d5
AnonDev <85408287+anondev-sudo@users.noreply.github.com>
2021-11-13 15:08:30 +0100

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -416,6 +416,26 @@
         }
     })
     @endif
+    
+const submitDeletionForm = (e, form, text) => {
+e.preventDefault();
+     Swal.fire({
+        title: "Are you sure?",
+        html: `Are you sure you wish to delete this ${text}?<br>This is an irreversible action, all files will be removed.`,
+        icon: 'warning',
+        confirmButtonColor: '#d9534f',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+              if (result.isConfirmed) {
+                  return form.submit()
+              } else {
+                  return Swal.fire('Canceled ...', `${text} deletion has been canceled.`, 'info')
+              }
+          });    
+}
 </script>
 </body>
 </html>
