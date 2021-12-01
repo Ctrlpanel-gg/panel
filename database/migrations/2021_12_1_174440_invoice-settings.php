a2ab45d28cca65c8982394e5f24f3a85eab3714e
WBLKLeipe <ownerdennis8@gmail.com>
2021-12-01 12:27:49 +0100

    fix .env.example and migration

--- database/migrations/2021_12_1_174440_invoice-settings.php
+++ database/migrations/2021_12_1_174440_invoice-settings.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class InvoiceSettings extends Migration
@@ -20,9 +21,19 @@ class InvoiceSettings extends Migration
             $table->string('company_phone')->nullable();
             $table->string('company_vat')->nullable();
             $table->string('company_mail')->nullable();
-            $table->string('company_web')->nullable();
+            $table->string('company_web')->nullable()->default(env("APP_URL",""));
+            $table->string('invoice_prefix')->nullable();
             $table->timestamps();
         });
+
+        DB::table('invoice_settings')->insert(
+            array(
+                'company_name' => env("APP_NAME","MyCompany"),
+                'company_web' => env("APP_URL",""),
+                'invoice_prefix' => "INV"
+
+            )
+        );
     }
 
     /**
