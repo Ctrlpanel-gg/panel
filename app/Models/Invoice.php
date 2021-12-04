1d9b0546dd4cdfdcd4221547d97762bcb4396de9
1day2die <ownerdennis8@gmail.com>
2021-12-04 16:32:25 +0100

    PSR-4 + cleanup

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Hidehalo\Nanoid\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class invoice extends Model
+class Invoice extends Model
 {
     use HasFactory;
 
