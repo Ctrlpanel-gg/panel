88d460c661644fe44729dce2a6f2810d5e41521a
Dennis <ownerdennis8@gmail.com>
2021-12-20 19:23:41 +0100

    Update confirmed.blade.php

--- resources/views/mail/payment/confirmed.blade.php
+++ resources/views/mail/payment/confirmed.blade.php
@@ -4,13 +4,13 @@
 
 # Details
 ___
-### {{__('Payment ID')}}': **{{$payment->id}}**<br>
-### {{__('Status')}}':     **{{$payment->status}}**<br>
-### {{__('Price')}}':      **{{$payment->formatToCurrency($payment->total_price)}}**<br>
-### {{__('Type')}}':       **{{$payment->type}}**<br>
-### {{__('Amount')}}':     **{{$payment->amount}}**<br>
-### {{__('Balance')}}':    **{{$payment->user->credits}}**<br>
-### {{__('User ID')}}':    **{{$payment->user_id}}**<br>
+### {{__('Payment ID')}}: **{{$payment->id}}**<br>
+### {{__('Status')}}:     **{{$payment->status}}**<br>
+### {{__('Price')}}:      **{{$payment->formatToCurrency($payment->total_price)}}**<br>
+### {{__('Type')}}:       **{{$payment->type}}**<br>
+### {{__('Amount')}}:     **{{$payment->amount}}**<br>
+### {{__('Balance')}}:    **{{$payment->user->credits}}**<br>
+### {{__('User ID')}}:    **{{$payment->user_id}}**<br>
 
 <br>
 {{__('Thanks')}},<br>
