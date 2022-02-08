54b55c0f5871e064e27e164b5f540e9cbefb4d4b
1Day <ownerdennis8@gmail.com>
2022-02-08 08:13:13 +0100

    installer: Better logging

--- public/install/functions.php
+++ public/install/functions.php
@@ -130,4 +130,17 @@ function run_console($command)
     return shell_exec($cmd);
 }
 
+function wh_log($log_msg)
+{
+    $log_filename = "log";
+    if (!file_exists($log_filename))
+    {
+        // create directory/folder uploads.
+        mkdir($log_filename, 0777, true);
+    }
+    $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
+    // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
+    file_put_contents($log_file_data, "[".date('h:i:s')."] " . $log_msg . "\n", FILE_APPEND);
+}
+
 ?>
