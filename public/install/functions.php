d6935482e68150fb60aff01aa4e40d5a9f0ebcde
1Day <ownerdennis8@gmail.com>
2022-02-15 16:05:46 +0100

    small loggin fixes

--- public/install/functions.php
+++ public/install/functions.php
@@ -130,12 +130,12 @@ function run_console($command)
 
 function wh_log($log_msg)
 {
-    $log_filename = "log";
+    $log_filename = "logs";
     if (!file_exists($log_filename)) {
         // create directory/folder uploads.
         mkdir($log_filename, 0777, true);
     }
-    $log_file_data = $log_filename . '/log_' . date('d-M-Y') . '.log';
+    $log_file_data = $log_filename . '/installer.log';
     // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
     file_put_contents($log_file_data, "[" . date('h:i:s') . "] " . $log_msg . "\n", FILE_APPEND);
 }
