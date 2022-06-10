4feac626cfc907d133599e13fbff77e189c893ef
Vermium Sifell <vermium@zyner.org>
2022-06-10 17:57:50 +0200

    📱 updated to new logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -182,8 +182,8 @@
             <!-- Brand Logo -->
             <a href="{{ route('home') }}" class="brand-link">
                 <img width="64" height="64"
-                    src="{{ \Illuminate\Support\Facades\Storage::disk('public')->exists('icon.png')? asset('storage/icon.png'): asset('images/bitsec.png') }}"
-                    alt="{{ config('app.name', 'Laravel') }} Logo" class="brand-image img-circle"
+                    src="{{ \Illuminate\Support\Facades\Storage::disk('public')->exists('icon.png')? asset('storage/icon.png'): asset('images/controlpanel_logo.png') }}"
+                    alt="{{ config('app.name', 'Controlpanel.gg') }} Logo" class="brand-image img-circle"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
             </a>
