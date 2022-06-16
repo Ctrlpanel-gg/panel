b726326e99e00cbfbefb0e85625a075013c9e984
IceToast <social@icetoast.cloud>
2022-06-16 12:01:53 +0200

    feat: 📝 Updated ProductSeeder

--- database/seeders/Seeds/ProductSeeder.php
+++ database/seeders/Seeds/ProductSeeder.php
@@ -16,29 +16,32 @@ class ProductSeeder extends Seeder
     {
         Product::create([
             'name' => 'Starter',
-            'description' => '64MB Ram, 1GB Disk, 1 Database, 140 credits monthly',
+            'description' => '64MB Ram, 1GB Disk, 1 Database, 140 credits hourly',
             'price' => 140,
             'memory' => 64,
             'disk' => 1000,
-            'databases' => 1
+            'databases' => 1,
+            'billing_period' => 'hourly'
         ]);
 
         Product::create([
             'name' => 'Standard',
-            'description' => '128MB Ram, 2GB Disk, 2 Database,  210 credits monthly',
+            'description' => '128MB Ram, 2GB Disk, 2 Database,  210 credits hourly',
             'price' => 210,
             'memory' => 128,
             'disk' => 2000,
-            'databases' => 2
+            'databases' => 2,
+            'billing_period' => 'hourly'
         ]);
 
         Product::create([
             'name' => 'Advanced',
-            'description' => '256MB Ram, 5GB Disk, 5 Database,  280 credits monthly',
+            'description' => '256MB Ram, 5GB Disk, 5 Database,  280 credits hourly',
             'price' => 280,
             'memory' => 256,
             'disk' => 5000,
-            'databases' => 5
+            'databases' => 5,
+            'billing_period' => 'hourly'
         ]);
     }
 }
