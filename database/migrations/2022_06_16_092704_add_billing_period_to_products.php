8f38b9e023c3b1f17b93fc5372cfc4423c069c33
IceToast <social@icetoast.cloud>
2022-06-16 11:32:00 +0200

    feat: ✨ Added Migration -> Column Billing Period

---
+++ database/migrations/2022_06_16_092704_add_billing_period_to_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddBillingPeriodToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('billing_period')->default("hourly");
+        });
+
+        DB::statement('UPDATE products SET billing_period="hourly"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('billing_period');
+        });
+    }
+}
