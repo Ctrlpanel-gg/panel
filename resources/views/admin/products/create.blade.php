4581b018b0bf57a527b813f4e1f902c84ebcbb86
IceToast <social@icetoast.cloud>
2022-06-16 12:27:53 +0200

    feat: ✨ Added billing period to edit, create and show of products

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -117,6 +117,20 @@
                                         </div>
 
                                         <div class="form-group">
+                                            <label for="allocations">{{__('Allocations')}}</label>
+                                            <input value="{{$product->allocations ?? old('allocations') ?? 0}}"
+                                                   id="allocations" name="allocations"
+                                                   type="number"
+                                                   class="form-control @error('allocations') is-invalid @enderror"
+                                                   required="required">
+                                            @error('allocations')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
                                             <label for="description">{{__('Description')}} <i data-toggle="popover"
                                                                                               data-trigger="hover"
                                                                                               data-content="{{__('This is what the users sees')}}"
@@ -142,6 +156,34 @@
                                                    class="form-control @error('disk') is-invalid @enderror"
                                                    required="required">
                                             @error('disk')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="billing_period">{{__('Billing Period')}} <i
+                                                    data-toggle="popover" data-trigger="hover"
+                                                    data-content="{{__('Period when the user will be charged for the given price')}}"
+                                                    class="fas fa-info-circle"></i></label>
+
+                                            <select id="billing_period" style="width:100%" class="custom-select" name="billing_period" required
+                                                autocomplete="off" @error('billing_period') is-invalid @enderror>
+                                                    <option value="hourly" @if ($product->billing_period == 'hourly') selected
+                                                    @endif>
+                                                        {{__('Hourly')}}
+                                                    </option>
+                                                    <option value="daily" @if ($product->billing_period  == 'daily') selected
+                                                    @endif>
+                                                        {{__('Daily')}}
+                                                    </option>
+                                                     <option value="monthly" @if ($product->billing_period  == 'monthly') selected
+                                                     @endif>
+                                                        {{__('Monthly')}}
+                                                    </option>
+                                            </select>
+                                            @error('billing_period')
                                             <div class="invalid-feedback">
                                                 {{ $message }}
                                             </div>
@@ -200,19 +242,6 @@
                                                    class="form-control @error('backups') is-invalid @enderror"
                                                    required="required">
                                             @error('backups')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="allocations">{{__('Allocations')}}</label>
-                                            <input value="{{$product->allocations ?? old('allocations') ?? 0}}"
-                                                   id="allocations" name="allocations"
-                                                   type="number"
-                                                   class="form-control @error('allocations') is-invalid @enderror"
-                                                   required="required">
-                                            @error('allocations')
                                             <div class="invalid-feedback">
                                                 {{ $message }}
                                             </div>
