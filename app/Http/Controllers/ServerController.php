e3e7329d312a6d9d73a52db4f1976b6917736f36
IceToast <social@icetoast.cloud>
2022-07-28 23:33:04 +0200

    fix: 🐛 See last commit

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -106,11 +106,12 @@ class ServerController extends Controller
         if (FacadesRequest::has("product")) {
             $product = Product::findOrFail(FacadesRequest::input("product"));
 
+            error_log(Auth::user()->credits);
+            error_log($product->price);
+
+            error_log(Auth::user()->credits < $product->price ? "true" : "false");
             if (
-                Auth::user()->credits <
-                ($product->minimum_credits == -1
-                    ? config('SETTINGS::USER:MINIMUM_REQUIRED_CREDITS_TO_MAKE_SERVER', 50)
-                    : $product->minimum_credits) ||
+                Auth::user()->credits < $product->minimum_credits ||
                 Auth::user()->credits < $product->price
             ) {
                 return redirect()->route('servers.index')->with('error', "You do not have the required amount of " . CREDITS_DISPLAY_NAME . " to use this product!");
