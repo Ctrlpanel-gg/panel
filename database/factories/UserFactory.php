c15d0f4ef680d4fabe03a0471697e4221250d42a
1day2die <ownerdennis8@gmail.com>
2022-11-29 09:20:24 +0100

    Fix Ticket System

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,12 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'credits' => $this->faker->numberBetween(0,1500),
+            'credits' => $this->faker->numberBetween(0, 1500),
             'last_seen' => $this->faker->dateTimeBetween(now(), '+30 days'),
             'email_verified_at' => $this->faker->dateTimeBetween('-30 days', now()),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'email_verified' => true,
         ];
     }
 }
