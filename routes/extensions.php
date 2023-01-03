4041955296724849da8273342ce37e6370be8e5d
IceToast <>
2023-01-03 13:49:37 +0100

    feat: ✨ Added Extension routes

---
+++ routes/extensions.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'extensions'], function () {
+    $extensions = glob(app_path() . '/Extensions/*', GLOB_ONLYDIR);
+    foreach ($extensions as $extension) {
+        $routesFile = $extension . '/routes.php';
+        if (file_exists($routesFile)) {
+            include_once $routesFile;
+        }
+    }
+});
