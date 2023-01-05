daff0fc566732cd52fdd7068bdb8d782f2bbc38e
Shift <shift@laravelshift.com>
2023-01-05 17:02:12 +0000

    Streamline `$commands` property

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,15 +9,6 @@ use Illuminate\Support\Facades\Storage;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
