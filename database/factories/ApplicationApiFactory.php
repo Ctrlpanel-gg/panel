abb28548f1adcf1b22e2e3f7644aaa9174aeec3b
Shift <shift@laravelshift.com>
2023-01-05 17:02:37 +0000

    Remove unnecessary `$model` property

--- database/factories/ApplicationApiFactory.php
+++ database/factories/ApplicationApiFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ApplicationApiFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = ApplicationApi::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
--- database/factories/ServerFactory.php
+++ database/factories/ServerFactory.php
@@ -10,13 +10,6 @@ use Illuminate\Support\Str;
 class ServerFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Server::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
