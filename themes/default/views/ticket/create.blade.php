57577c95b44d66d96fedeaff1beff9eaa19f5a71
1day2die <ownerdennis8@gmail.com>
2023-02-01 10:06:15 +0100

    Ticket Tweaks

--- themes/default/views/ticket/create.blade.php
+++ themes/default/views/ticket/create.blade.php
@@ -46,14 +46,14 @@
                                 </div>
                                 @if ($servers->count() >= 1)
                                 <div class="form-group col-sm-12 {{ $errors->has('server') ? ' has-error' : '' }}">
-                                    <label for="server" class="control-label">Servers</label>
+                                    <label for="server" class="control-label">{{__("Server")}}</label>
                                     <select id="server" type="server" class="form-control" name="server">
-                                        <option value="">Select Servers</option>
+                                        <option value="">{{__("Select Servers")}}</option>
                                         @foreach ($servers as $server)
                                         <option value="{{ $server->id }}">{{ $server->name }}</option>
                                         @endforeach
                                     </select>
-    
+
                                     @if ($errors->has('category'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('ticketcategory') }}</strong>
@@ -62,14 +62,14 @@
                                 </div>
                                 @endif
                                 <div class="form-group col-sm-12 {{ $errors->has('ticketcategory') ? ' has-error' : '' }}">
-                                    <label for="ticketcategory" class="control-label">Category</label>
-                                    <select id="ticketcategory" type="ticketcategory" class="form-control" name="ticketcategory">
-                                        <option value="">Select Category</option>
+                                    <label for="ticketcategory" class="control-label">{{__("Category")}}</label>
+                                    <select id="ticketcategory" type="ticketcategory" class="form-control" required name="ticketcategory">
+                                        <option value="" disabled selected>{{__("Select Category")}}</option>
                                         @foreach ($ticketcategories as $ticketcategory)
                                         <option value="{{ $ticketcategory->id }}">{{ $ticketcategory->name }}</option>
                                         @endforeach
                                     </select>
-    
+
                                     @if ($errors->has('category'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('ticketcategory') }}</strong>
@@ -79,10 +79,10 @@
                                 <div class="form-group col-sm-12 {{ $errors->has('priority') ? ' has-error' : '' }}">
                                     <label for="priority" class="control-label">Priority</label>
                                     <select id="priority" type="" class="form-control" name="priority">
-                                        <option value="">Select Priority</option>
-                                        <option value="Low">Low</option>
-                                        <option value="Medium">Medium</option>
-                                        <option value="High">High</option>
+                                        <option value="" disabled selected>{{__("Select Priority")}}</option>
+                                        <option value="Low">{{__("Low")}}</option>
+                                        <option value="Medium">{{__("Medium")}}</option>
+                                        <option value="High">{{__("High")}}</option>
                                     </select>
                                     @if ($errors->has('priority'))
                                     <span class="help-block">
@@ -124,12 +124,12 @@
     </section>
     <!-- END CONTENT -->
     <script type="text/javascript">
-     $(".ticket-form").submit(function (e) {        
-         
-         $(".ticket-once").attr("disabled", true);     
+     $(".ticket-form").submit(function (e) {
+
+         $(".ticket-once").attr("disabled", true);
          return true;
      })
-        
+
     </script>
 @endsection
 
