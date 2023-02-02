65d0d550110eb11c28883c19e66ce706ff3e73f6
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2023-02-02 15:08:34 +0000

    Add missing keys

--- database/settings/2023_02_01_181950_create_server_settings.php
+++ database/settings/2023_02_01_181950_create_server_settings.php
@@ -9,6 +9,8 @@ class CreateServerSettings extends SettingsMigration
     {
         // Get the user-set configuration values from the old table.
         $this->migrator->add('server.allocation_limit', ($this->getOldValue('SETTINGS::SERVER:ALLOCATION_LIMIT') != null) ?: 200);
+        $this->migrator->add('server.creation_enabled', ($this->getOldValue('SETTINGS::SYSTEM:CREATION_OF_NEW_SERVERS') != null) ?: true);
+        $this->migrator->add('server.enable_upgrade', ($this->getOldValue('SETTINGS::SYSTEM:ENABLE_UPGRADE') != null) ?: false);
     }
 
     public function getOldValue(string $key)
