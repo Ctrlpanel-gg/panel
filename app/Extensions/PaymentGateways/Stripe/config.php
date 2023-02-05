b7a765bd8ca5234579b671f5a58396d0109b1f50
IceToast <social@icetoast.de>
2023-02-05 21:07:13 +0100

    fix: 🚑️ Stripe extensions enable flag

--- app/Extensions/PaymentGateways/Stripe/config.php
+++ app/Extensions/PaymentGateways/Stripe/config.php
@@ -10,6 +10,6 @@ function getConfig()
         "RoutesIgnoreCsrf" => [
             "payment/StripeWebhooks",
         ],
-        "enabled" => config('SETTINGS::PAYMENTS:STRIPE:SECRET') && config('SETTINGS::PAYMENTS:STRIPE:CLIENT_ID'),
+        "enabled" => config('SETTINGS::PAYMENTS:STRIPE:SECRET') && config('SETTINGS::PAYMENTS:STRIPE:ENDPOINT_SECRET'),
     ];
 }
