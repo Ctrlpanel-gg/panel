66f5b9ff14fc9f1782989ae7ffc86e381b7feb47
IceToast <social@icetoast.de>
2023-02-09 22:34:34 +0100

    feat: ✨ Add InputData to all settings & Render correct inputs

--- app/Settings/ServerSettings.php
+++ app/Settings/ServerSettings.php
@@ -18,4 +18,35 @@ class ServerSettings extends Settings
     {
         return 'server';
     }
+
+    /**
+     * Summary of optionTypes
+     * Only used for the settings page
+     * @return array<array<'type'|'label'|'description'|'options', string|bool|float|int|array<string, string>>>
+     */
+    public static function getOptionInputData()
+    {
+        return [
+            'allocation_limit' => [
+                'label' => 'Allocation Limit',
+                'type' => 'number',
+                'description' => 'The maximum amount of allocations to pull per node for automatic deployment, if more allocations are being used than this limit is set to, no new servers can be created.',
+            ],
+            'creation_enabled' => [
+                'label' => 'Creation Enabled',
+                'type' => 'boolean',
+                'description' => 'Whether or not users can create servers.',
+            ],
+            'enable_upgrade' => [
+                'label' => 'Enable Upgrade',
+                'type' => 'boolean',
+                'description' => 'Whether or not users can upgrade their servers.',
+            ],
+            'charge_first_hour' => [
+                'label' => 'Charge First Hour',
+                'type' => 'boolean',
+                'description' => 'Whether or not the first hour of a server is charged.',
+            ],
+        ];
+    }
 }
