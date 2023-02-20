a6a729a065921e8059234f190eed560766f954ae
IceToast <social@icetoast.de>
2023-02-20 13:33:00 +0100

    fix: 🐛 Remove quotes from strings

--- database/settings/2023_02_01_164731_create_general_settings.php
+++ database/settings/2023_02_01_164731_create_general_settings.php
@@ -109,6 +109,11 @@ class CreateGeneralSettings extends SettingsMigration
             return null;
         }
 
+        // remove the quotes from the string
+        if (substr($new_value->payload, 0, 1) === '"' && substr($new_value->payload, -1) === '"') {
+            return substr($new_value->payload, 1, -1);
+        }
+
         return $new_value->payload;
     }
 
