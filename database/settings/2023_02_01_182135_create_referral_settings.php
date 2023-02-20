ecab05d5a2a224778e5198fb22eb895b7c20beab
IceToast <social@icetoast.de>
2023-02-20 13:33:00 +0100

    fix: 🐛 Remove quotes from strings

--- database/settings/2023_02_01_182135_create_referral_settings.php
+++ database/settings/2023_02_01_182135_create_referral_settings.php
@@ -76,6 +76,11 @@ class CreateReferralSettings extends SettingsMigration
             return null;
         }
 
+        // remove the quotes from the string
+        if (substr($new_value->payload, 0, 1) === '"' && substr($new_value->payload, -1) === '"') {
+            return substr($new_value->payload, 1, -1);
+        }
+
         return $new_value->payload;
     }
 
