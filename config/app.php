fc575a95547f8b5e73d986b88e9440f139053c5d
Dennis <ownerdennis8@gmail.com>
2023-02-23 09:37:56 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'version' => '0.9.2',
+    'version' => '0.9.3',
 
     /*
     |--------------------------------------------------------------------------
