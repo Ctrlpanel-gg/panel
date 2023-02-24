aff8f97d172c44f7dda47b692998dd471ec71b34
IceToast <54889359+IceToast@users.noreply.github.com>
2023-02-24 14:02:53 +0100

    Revert "feat: implement laravel-settings"
    This reverts commit 2229586b58e68c99f6d0c0986d9749c88a2d6dd3.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Settings;
 use App\Models\UsefulLink;
 use Exception;
 use Illuminate\Pagination\Paginator;
@@ -63,89 +64,91 @@ class AppServiceProvider extends ServiceProvider
         }
 
         //only run if the installer has been executed
-        // try {
-        //     $settings = Settings::all();
-        //     // Set all configs from database
-        //     foreach ($settings as $setting) {
-        //         config([$setting->key => $setting->value]);
-        //     }
+        try {
+            $settings = Settings::all();
+            // Set all configs from database
+            foreach ($settings as $setting) {
+                config([$setting->key => $setting->value]);
+            }
 
-        //     if(!file_exists(base_path('themes') . "/" . config("SETTINGS::SYSTEM:THEME"))){
-        //         config(['SETTINGS::SYSTEM:THEME' => "default"]);
-        //     }
+            if (!file_exists(base_path('themes') . "/" . config("SETTINGS::SYSTEM:THEME"))) {
+                config(['SETTINGS::SYSTEM:THEME' => "default"]);
+            }
 
-        //     if(config('SETTINGS::SYSTEM:THEME') !== config('theme.active')){
-        //         Theme::set(config("SETTINGS::SYSTEM:THEME"), "default");
-        //     }
+            if (config('SETTINGS::SYSTEM:THEME') && config('SETTINGS::SYSTEM:THEME') !== config('theme.active')) {
+                Theme::set(config("SETTINGS::SYSTEM:THEME", "default"), "default");
+            } else {
+                Theme::set("default", "default");
+            }
 
-        //     // Set Mail Config
-        //     //only update config if mail settings have changed in DB
-        //     if (
-        //         config('mail.default') != config('SETTINGS:MAIL:MAILER') ||
-        //         config('mail.mailers.smtp.host') != config('SETTINGS:MAIL:HOST') ||
-        //         config('mail.mailers.smtp.port') != config('SETTINGS:MAIL:PORT') ||
-        //         config('mail.mailers.smtp.username') != config('SETTINGS:MAIL:USERNAME') ||
-        //         config('mail.mailers.smtp.password') != config('SETTINGS:MAIL:PASSWORD') ||
-        //         config('mail.mailers.smtp.encryption') != config('SETTINGS:MAIL:ENCRYPTION') ||
-        //         config('mail.from.address') != config('SETTINGS:MAIL:FROM_ADDRESS') ||
-        //         config('mail.from.name') != config('SETTINGS:MAIL:FROM_NAME')
-        //     ) {
-        //         config(['mail.default' => config('SETTINGS::MAIL:MAILER')]);
-        //         config(['mail.mailers.smtp' => [
-        //             'transport' => 'smtp',
-        //             'host' => config('SETTINGS::MAIL:HOST'),
-        //             'port' => config('SETTINGS::MAIL:PORT'),
-        //             'encryption' => config('SETTINGS::MAIL:ENCRYPTION'),
-        //             'username' => config('SETTINGS::MAIL:USERNAME'),
-        //             'password' => config('SETTINGS::MAIL:PASSWORD'),
-        //             'timeout' => null,
-        //             'auth_mode' => null,
-        //         ]]);
-        //         config(['mail.from' => ['address' => config('SETTINGS::MAIL:FROM_ADDRESS'), 'name' => config('SETTINGS::MAIL:FROM_NAME')]]);
+            // Set Mail Config
+            //only update config if mail settings have changed in DB
+            if (
+                config('mail.default') != config('SETTINGS:MAIL:MAILER') ||
+                config('mail.mailers.smtp.host') != config('SETTINGS:MAIL:HOST') ||
+                config('mail.mailers.smtp.port') != config('SETTINGS:MAIL:PORT') ||
+                config('mail.mailers.smtp.username') != config('SETTINGS:MAIL:USERNAME') ||
+                config('mail.mailers.smtp.password') != config('SETTINGS:MAIL:PASSWORD') ||
+                config('mail.mailers.smtp.encryption') != config('SETTINGS:MAIL:ENCRYPTION') ||
+                config('mail.from.address') != config('SETTINGS:MAIL:FROM_ADDRESS') ||
+                config('mail.from.name') != config('SETTINGS:MAIL:FROM_NAME')
+            ) {
+                config(['mail.default' => config('SETTINGS::MAIL:MAILER')]);
+                config(['mail.mailers.smtp' => [
+                    'transport' => 'smtp',
+                    'host' => config('SETTINGS::MAIL:HOST'),
+                    'port' => config('SETTINGS::MAIL:PORT'),
+                    'encryption' => config('SETTINGS::MAIL:ENCRYPTION'),
+                    'username' => config('SETTINGS::MAIL:USERNAME'),
+                    'password' => config('SETTINGS::MAIL:PASSWORD'),
+                    'timeout' => null,
+                    'auth_mode' => null,
+                ]]);
+                config(['mail.from' => ['address' => config('SETTINGS::MAIL:FROM_ADDRESS'), 'name' => config('SETTINGS::MAIL:FROM_NAME')]]);
 
-        //         Artisan::call('queue:restart');
-        //     }
+                Artisan::call('queue:restart');
+            }
 
-        //     // Set Recaptcha API Config
-        //     // Load recaptcha package if recaptcha is enabled
-        //     if (config('SETTINGS::RECAPTCHA:ENABLED') == 'true') {
-        //         $this->app->register(\Biscolab\ReCaptcha\ReCaptchaServiceProvider::class);
-        //     }
+            // Set Recaptcha API Config
+            // Load recaptcha package if recaptcha is enabled
+            if (config('SETTINGS::RECAPTCHA:ENABLED') == 'true') {
+                $this->app->register(\Biscolab\ReCaptcha\ReCaptchaServiceProvider::class);
+            }
 
-        //     //only update config if recaptcha settings have changed in DB
-        //     if (
-        //         config('recaptcha.api_site_key') != config('SETTINGS::RECAPTCHA:SITE_KEY') ||
-        //         config('recaptcha.api_secret_key') != config('SETTINGS::RECAPTCHA:SECRET_KEY')
-        //     ) {
-        //         config(['recaptcha.api_site_key' => config('SETTINGS::RECAPTCHA:SITE_KEY')]);
-        //         config(['recaptcha.api_secret_key' => config('SETTINGS::RECAPTCHA:SECRET_KEY')]);
+            //only update config if recaptcha settings have changed in DB
+            if (
+                config('recaptcha.api_site_key') != config('SETTINGS::RECAPTCHA:SITE_KEY') ||
+                config('recaptcha.api_secret_key') != config('SETTINGS::RECAPTCHA:SECRET_KEY')
+            ) {
+                config(['recaptcha.api_site_key' => config('SETTINGS::RECAPTCHA:SITE_KEY')]);
+                config(['recaptcha.api_secret_key' => config('SETTINGS::RECAPTCHA:SECRET_KEY')]);
 
-        //         Artisan::call('config:clear');
-        //         Artisan::call('cache:clear');
-        //     }
+                Artisan::call('config:clear');
+                Artisan::call('cache:clear');
+            }
 
-        //     try {
-        //         $stringfromfile = file(base_path().'/.git/HEAD');
+            try {
+                $stringfromfile = file(base_path() . '/.git/HEAD');
 
-        //         $firstLine = $stringfromfile[0]; //get the string from the array
+                $firstLine = $stringfromfile[0]; //get the string from the array
 
-        //         $explodedstring = explode('/', $firstLine, 3); //seperate out by the "/" in the string
+                $explodedstring = explode('/', $firstLine, 3); //seperate out by the "/" in the string
 
-        //         $branchname = $explodedstring[2]; //get the one that is always the branch name
-        //     } catch (Exception $e) {
-        //         $branchname = 'unknown';
-        //         Log::notice($e);
-        //     }
-        //     config(['BRANCHNAME' => $branchname]);
+                $branchname = $explodedstring[2]; //get the one that is always the branch name
+            } catch (Exception $e) {
+                $branchname = 'unknown';
+                Log::notice($e);
+            }
+            config(['BRANCHNAME' => $branchname]);
 
-        //     // Set Discord-API Config
-        //     config(['services.discord.client_id' => config('SETTINGS::DISCORD:CLIENT_ID')]);
-        //     config(['services.discord.client_secret' => config('SETTINGS::DISCORD:CLIENT_SECRET')]);
-        // } catch (Exception $e) {
-        //     error_log('Settings Error: Could not load settings from database. The Installation probably is not done yet.');
-        //     error_log($e);
-        //     Log::error('Settings Error: Could not load settings from database. The Installation probably is not done yet.');
-        //     Log::error($e);
-        // }
+            // Set Discord-API Config
+            config(['services.discord.client_id' => config('SETTINGS::DISCORD:CLIENT_ID')]);
+            config(['services.discord.client_secret' => config('SETTINGS::DISCORD:CLIENT_SECRET')]);
+        } catch (Exception $e) {
+            error_log('Settings Error: Could not load settings from database. The Installation probably is not done yet.');
+            error_log($e);
+            Log::error('Settings Error: Could not load settings from database. The Installation probably is not done yet.');
+            Log::error($e);
+        }
     }
 }
