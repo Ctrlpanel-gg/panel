1c353ed5e6865bcba0cf646a21ac7c4f7c9a3cc6
IceToast <54889359+IceToast@users.noreply.github.com>
2023-02-24 14:02:53 +0100

    Revert "Ptero API keys are now encrypted."
    This reverts commit 1086dd85a490038b9fbf39ce8a7fcce40b25c87a.

--- database/settings/2023_02_01_181334_create_pterodactyl_settings.php
+++ database/settings/2023_02_01_181334_create_pterodactyl_settings.php
@@ -8,8 +8,8 @@ class CreatePterodactylSettings extends SettingsMigration
     public function up(): void
     {
         // Get the user-set configuration values from the old table.
-        $this->migrator->addEncrypted('pterodactyl.admin_token', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:TOKEN') != null) ?: env('PTERODACTYL_TOKEN', ''));
-        $this->migrator->addEncrypted('pterodactyl.user_token', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:ADMIN_USER_TOKEN') != null) ?: '');
+        $this->migrator->add('pterodactyl.admin_token', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:TOKEN') != null) ?: env('PTERODACTYL_TOKEN', ''));
+        $this->migrator->add('pterodactyl.user_token', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:ADMIN_USER_TOKEN') != null) ?: '');
         $this->migrator->add('pterodactyl.panel_url', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:URL') != null) ?: env('PTERODACTYL_URL', ''));
         $this->migrator->add('pterodactyl.per_page_limit', ($this->getOldValue('SETTINGS::SYSTEM:PTERODACTYL:PER_PAGE_LIMIT') != null) ?: 200);
     }
