ad9e33d0d7ea94659bb6dfd08b77ceab1cfe8a2e
Dennis <ownerdennis8@gmail.com>
2023-03-09 12:43:38 +0100

    Update web_routes.php

--- app/Extensions/PaymentGateways/PayPal/web_routes.php
+++ app/Extensions/PaymentGateways/PayPal/web_routes.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 include_once(__DIR__ . '/index.php');
 
 Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('payment/PaypalPay/{shopProduct}', function () {
+    Route::get('payment/PayPalPay/{shopProduct}', function () {
         PaypalPay(request());
-    })->name('payment.PaypalPay');
+    })->name('payment.PayPalPay');
 
     Route::get(
         'payment/PayPalSuccess',
