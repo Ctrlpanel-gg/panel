60a4c4f2a6581c7444528acf2eafef0145cc5259
SwitchAlpha <48888576+SwitchAlpha@users.noreply.github.com>
2023-05-17 23:24:22 +0300

    Update currency_codes.php

--- config/currency_codes.php
+++ config/currency_codes.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'AUD', 'BRL', 'CAD', 'CNY', 'CZK', 'DKK', 'EUR', 'HKD', 'HUF', 'ILS', 'JPY', 'MYR', 'MXN', 'TWD', 'NZD', 'NOK', 'PHP', 'PLN', 'GBP', 'RUB', 'SGD', 'SEK', 'CHF', 'THB', 'USD',
+    'AUD', 'BRL', 'CAD', 'CNY', 'CZK', 'DKK', 'EUR', 'HKD', 'HUF', 'ILS', 'JPY', 'MYR', 'MXN', 'TWD', 'NZD', 'NOK', 'PHP', 'PLN', 'GBP', 'RUB', 'SGD', 'SEK', 'CHF', 'THB', 'USD','TRY,
 ];
