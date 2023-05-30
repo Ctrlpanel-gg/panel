cd9baa16b18348b2f4daf149abaaee81f63d29c5
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2023-05-30 14:43:21 +0000

    refactor: :art: Rewrite the logic of sending server suspension mails.
    Change the logic so that a job is used in combination with notifications.

---
+++ app/Jobs/ServersSuspendedJob.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Notifications\ServersSuspendedNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+
+class ServersSuspendedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private array $usersToNotify;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $usersToNotify)
+    {
+        $this->usersToNotify = $usersToNotify;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //$users = User::whereIn('id', $this->usersToNotify)->get();
+        User::whereIn('id', $this->usersToNotify)->chunk(100, function($users) {
+            $users->each(function ($user) {
+                Notification::send($user, new ServersSuspendedNotification());
+            });
+        });
+    }
+}
