aa3e8037e5aebdcaf98008897b04a28fa2d7bc84
MrWeez <64205495+MrWeez@users.noreply.github.com>
2023-06-07 19:52:34 +0300

    The ability to translate the referral link field

--- themes/default/views/profile/index.blade.php
+++ themes/default/views/profile/index.blade.php
@@ -105,14 +105,14 @@
                                             <div class="mt-1">
                                                     <span class="badge badge-success"><i
                                                             class="fa fa-user-check mr-2"></i>
-                                                        {{_("Referral URL")}} :
+                                                        {{__("Referral URL")}} :
                                                         <span onclick="onClickCopy()" id="RefLink" style="cursor: pointer;">
                                                             {{route("register")}}?ref={{$user->referral_code}}</span>
                                                     </span>
                                                 @else
                                                     <span class="badge badge-warning"><i
                                                             class="fa fa-user-check mr-2"></i>
-                                                        {{_("Make a purchase to reveal your referral-URL")}}</span>
+                                                        {{__("Make a purchase to reveal your referral-URL")}}</span>
                                         @endif
                                             </div>
                                         @endif
