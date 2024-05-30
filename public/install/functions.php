e564a3886062ba9d86312d05c7f8ec19dbf94aca
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2024-05-30 14:44:01 +0200

    Comment out everything in run_console command

--- public/install/functions.php
+++ public/install/functions.php
@@ -235,15 +235,20 @@ function run_console(string $command, array $descriptors = null, string $cwd = n
     wh_log('running command: ' . $command, 'debug');
 
     $path = dirname(__FILE__, 3);
-    $descriptors = $descriptors ?? [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
+    wh_log('running command: ' . "cd '$path' && bash -c '$command'", 'debug');
+
+//    $path = dirname(__FILE__, 3);
+//    $descriptors = $descriptors ?? [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
 //    if (file_exists('/.dockerenv')) {
-        $handle = proc_open("cd '$path' && bash -c '$command'", $descriptors, $pipes, $cwd, null, $options);
+//        $handle = proc_open("cd '$path' && bash -c '$command'", $descriptors, $pipes, $cwd, null, $options);
 //    } else {
 //        $handle = proc_open("cd '$path' && bash -c 'exec -a ServerCPP $command'", $descriptors, $pipes, $cwd, null, $options);
 //    }
 
-    $output = stream_get_contents($pipes[1]);
-    $exit_code = proc_close($handle);
+//    $output = stream_get_contents($pipes[1]);
+//    $exit_code = proc_close($handle);
+    $exit_code = 0;
+    $output = 'yippie';
 
     if ($exit_code > 0) {
         wh_log('command result: ' . $output, 'error');
