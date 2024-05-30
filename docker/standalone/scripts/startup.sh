a2e0109e9df3c00882dadcd3c73a27b086d5011c
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2024-05-30 15:42:52 +0200

    Update permissions in startup.sh

--- docker/standalone/scripts/startup.sh
+++ docker/standalone/scripts/startup.sh
@@ -30,11 +30,11 @@ if [ -z "$(ls -A /var/www/html)" ]; then
     log_message "Warning: project folder is empty. Copying default files..."
     # Copy everything from /var/default to /var/www/html
     cp -nr /var/default/. /var/www/html   # Use -n to avoid overwriting existing files
-    chown -R laravel:laravel /var/www/html/
-    chmod -R 755 /var/www/html
+#    chown -R laravel:laravel /var/www/html/
+#    chmod -R 755 /var/www/html
 fi
 
-#chmod -R 755 /var/www/html
+chmod -R 755 /var/www/html
 #chmod -R 777 $LOG_DIR
 
 # Check and copy default Nginx configuration if not exists
