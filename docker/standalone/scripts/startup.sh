9cffe584b0408f3fbfc4704b2e5dfb2d849f805f
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2024-06-02 09:23:09 +0200

    Run copy of project files every time

--- docker/standalone/scripts/startup.sh
+++ docker/standalone/scripts/startup.sh
@@ -25,16 +25,14 @@ log_message() {
     echo "$1"
 }
 
-mkdir /var/www/html/
-
 # Check if project folder is empty.
-if [ -z "$(ls -A /var/www/html)" ]; then
+#if [ -z "$(ls -A /var/www/html)" ]; then
     log_message "Warning: project folder is empty. Copying default files..."
     # Copy everything from /var/default to /var/www/html
     cp -nr /var/default/. /var/www/html   # Use -n to avoid overwriting existing files
     chown -R laravel:laravel /var/www/html/
     chmod -R 755 /var/www/html
-fi
+#fi
 
 # Copy .env file for it to be available when starting the Docker container (to be able to bind-mount it to the host, instead of the entire project folder)
 cp -n /var/default/.env.example /var/www/html/.env   # Use -n to avoid overwriting existing files
