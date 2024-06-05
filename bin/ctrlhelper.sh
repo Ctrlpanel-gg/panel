a1d2377c35983d2e6edb448a9ae060b109a82318
MrWeez <arsenyplis2018@gmail.com>
2024-06-05 23:53:57 +0000

    refactor: Added and corrected comments and code optimization

--- bin/ctrlhelper.sh
+++ bin/ctrlhelper.sh
@@ -6,7 +6,7 @@ readonly DEFAULT_DIR="/var/www/controlpanel/"
 cpgg_dir=""
 cli_mode="false"
 
-# Logo for CLI-GUI
+# CtrlPanel logo
 logo() {
     clear
     echo "    ________       ______                   __            "
@@ -18,84 +18,50 @@ logo() {
     echo ""
 }
 
-# Function for restoring terminal content after the script is completed or closed
-restore_terminal() {
-    tput rmcup
-    exit
-}
-
-# Setting root CtrlPanel directory using the --cpgg-dir=/path/to/folder parameter
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-    --cli)
-        cli_mode="true"
-        shift
-        ;;
-    --cpgg-dir=*)
-        cpgg_dir="${1#*=}"
-        shift
-
-        # Check if --cpgg-dir not empty
-        if [ -z "$cpgg_dir" ]; then
-            echo " Argument --cpgg-dir can't be empty!"
-            exit 1
-        fi
-        # Check if directory exists
-        if [ ! -d "$cpgg_dir" ]; then
-            echo " $cpgg_dir directory doesn't exist. Try again"
-            exit 1
-        fi
-        if [ ! -f "$cpgg_dir/config/app.php" ]; then
-            echo " $cpgg_dir is not a root CtrlPanel directory. Try again"
-            exit 1
-        fi
-        ;;
-    *)
-        shift
-        ;;
-    esac
-done
-
-# Do terminal actions only if $cli_mode = false
-if [ "$cli_mode" == "false" ]; then
+# Saving terminal content
+save_terminal() {
     # Restoring terminal content after ^C
     trap restore_terminal SIGINT
 
     # Save terminal
     tput smcup
-fi
+}
 
+# Restoring terminal content after the script is completed or closed
+restore_terminal() {
+    tput rmcup
+    exit
+}
+
+# Specifying CtrlPanel directory if it differs from the default one
 set_cpgg_dir() {
-
     local is_exists=""
     local is_cpgg_root=""
     local is_null=""
 
     if [ "$cli_mode" == "false" ]; then
-        # Set root CtrlPanel directory using CLI-GUI
-        ## If $DEFAULT_DIR doesn't exists and $cpgg_dir var isn't specified
         if [ ! -d "$DEFAULT_DIR" ] && [ -z "$cpgg_dir" ]; then
             while true; do
                 logo
-                # If $is_exists and $is_cpgg_root = null or $is_null = true show "Default wasn't found"
+
+                # Different message depending on the validation response
                 if [ -z "$is_exists" ] && [ -z "$is_cpgg_root" ] || [ "$is_null" == "true" ]; then
                     echo " Default directory wasn't found. Specify directory where your CtrlPanel is installed (e.g. /var/www/controlpanel)"
-                # If $is_exists = false show "Directory not exist"
                 elif [[ $is_exists == false ]]; then
-                    echo " $cpgg_dir directory doesn't exist. Try again"
-                # If $is_cpgg_root = false show "Not a root CtrlPanel"
+                    echo " Directory $cpgg_dir doesn't exist. Specify directory where your CtrlPanel is installed (e.g. /var/www/controlpanel)"
                 elif [[ $is_cpgg_root == false ]]; then
-                    echo " $cpgg_dir is not a root CtrlPanel directory. Try again"
+                    echo " $cpgg_dir is not a root CtrlPanel directory. Specify directory where your CtrlPanel is installed (e.g. /var/www/controlpanel)"
                 fi
 
-                # Read specified directory
-                read -rp " > " cpgg_dir
+                # Reading specified directory
+                read -rep " > " cpgg_dir
 
                 # Set all validation vars to null
                 is_null=""
                 is_exists=""
                 is_cpgg_root=""
 
+                # Validation of the entered directory
                 if [ "$cpgg_dir" != "" ]; then
                     if [ ! -d "$cpgg_dir" ]; then
                         is_null=""
@@ -115,23 +81,21 @@ set_cpgg_dir() {
             done
         fi
     else
-        # Set root CtrlPanel directory using in CLI mode
-        ## If $DEFAULT_DIR doesn't exists and $cpgg_dir var isn't specified
-        if [ ! -d "$DEFAULT_DIR" ] && [ -z "$cpgg_dir" ]; then
-            echo " Default directory wasn't found. Specify directory where your CtrlPanel is installed using --cpgg-dir=/path/to/cpgg argument"
-            exit 1
-        fi
+        # If user uses --cli flag, default directory doesn't exists and user did not specify the directory using --cpgg-dir then return an error and stop script
+        echo " Default directory wasn't found. Specify directory where your CtrlPanel is installed using --cpgg-dir argument"
+        exit 1
     fi
 }
 
-set_cpgg_dir
-
-# Getting curent CtrlPanel version
-PANEL_VER=$(grep -oP "'version' => '\K[^']+" "${cpgg_dir:-$DEFAULT_DIR}/config/app.php")
-readonly PANEL_VER
-# Getting latest CtrlPanel version
-PANEL_LATEST_VER=$(curl -s https://api.github.com/repos/ctrlpanel-gg/panel/tags | jq -r '.[0].name')
-readonly PANEL_LATEST_VER
+# Getting current and latest version of CtrlPanel
+get_version() {
+    # Curent CtrlPanel version
+    PANEL_VER=$(grep -oP "'version' => '\K[^']+" "${cpgg_dir:-$DEFAULT_DIR}/config/app.php")
+    readonly PANEL_VER
+    # Latest CtrlPanel version
+    PANEL_LATEST_VER=$(curl -s https://api.github.com/repos/ctrlpanel-gg/panel/tags | jq -r '.[0].name')
+    readonly PANEL_LATEST_VER
+}
 
 # Comparing current and latest versions
 ## 0 => Latest version is installed
@@ -165,19 +129,28 @@ version_compare() {
     return 0 
 }
 
-is_update_needed=$(version_compare "$PANEL_VER" "$PANEL_LATEST_VER")
-readonly is_update_needed
+# Checking if an update is needed
+## 0 => Latest version is installed
+## 1 => Update available
+## 2 => Version above the latest one is installed
+update_needed_checker() {
+    is_update_needed=$(version_compare "$PANEL_VER" "$PANEL_LATEST_VER")
+    readonly is_update_needed
+}
 
-# Logo with versions for CLI-GUI
+# Version block for logo
 logo_version() {
     echo " Script    version: $SCRIPT_VER"
     echo " CtrlPanel version: $PANEL_VER"
     echo ""
 }
 
-# Message about available Update
+# Message about update status for logo
 logo_message() {
-    if [[ $is_update_needed == 1 ]]; then
+    if [[ $is_update_needed == 0 ]]; then
+        echo " You are using the latest version! No update required."
+        echo ""
+    elif [[ $is_update_needed == 1 ]]; then
         echo " New version available! You can update right now by selecting \"Update\" option."
         echo ""
     elif [[ $is_update_needed == 2 ]]; then
@@ -186,7 +159,59 @@ logo_message() {
     fi
 }
 
+# Handling arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --cli)
+        cli_mode="true"
+        shift
+        ;;
+    --cpgg-dir=*)
+        cpgg_dir="${1#*=}"
+        shift
+
+        # Validation of specified directory
+        if [ "$cpgg_dir" != "" ]; then
+            if [ ! -d "$cpgg_dir" ]; then
+                echo " ERROR: Directory $cpgg_dir doesn't exist."
+                exit 1
+            else
+                if [ ! -f "$cpgg_dir/config/app.php" ]; then
+                    echo " ERROR: $cpgg_dir is not a root CtrlPanel directory."
+                    exit 1
+                else
+                    continue
+                fi
+            fi
+        else
+            echo " ERROR: Argument --cpgg-dir can't be empty!"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "ERROR: Argument $1 not exists. Use --help to display help menu"
+        exit 1
+        ;;
+    esac
+done
+
+# Save terminal only if $cli_mode = false
 if [ "$cli_mode" == "false" ]; then
+    save_terminal
+fi
+
+# Calling function to specify a directory
+set_cpgg_dir
+
+# Moving to the CtrlPanel directory
+cd "${cpgg_dir:-$DEFAULT_DIR}" || { echo " ERROR: An error occurred while trying to switch to the working directory. Please try to run the script again, if the error persists, inform the developers!"; exit 1; }
+
+# Main functions
+if [ "$cli_mode" == "false" ]; then
+
+    get_version
+    update_needed_checker
+
     # Main menu
     main_menu() {
         logo
@@ -223,6 +248,7 @@ if [ "$cli_mode" == "false" ]; then
         esac
     }
 
+    # Install dependencies menu
     menu_1() {
         logo
         echo " In dev"
@@ -230,6 +256,7 @@ if [ "$cli_mode" == "false" ]; then
         main_menu
     }
 
+    # Update menu
     menu_2() {
         logo
         echo " In dev"
@@ -237,6 +264,7 @@ if [ "$cli_mode" == "false" ]; then
         main_menu
     }
 
+    # Uninstall menu
     menu_3() {
         logo
         echo " In dev"
@@ -244,6 +272,7 @@ if [ "$cli_mode" == "false" ]; then
         main_menu
     }
 
+    # Info & Help menu
     menu_4() {
         logo
         echo " In dev"
@@ -251,9 +280,12 @@ if [ "$cli_mode" == "false" ]; then
         main_menu
     }
 
+    # Launch main munu
     main_menu
-fi
-# Restoring terminal after succes if $cli_mode = false
-if [ "$cli_mode" == "false" ]; then
+
+    # Restoring terminal after script end
     restore_terminal
+else
+    # Temporary function. In the future, all necessary actions in CLI mode will be performed here
+    echo " CLI mode commands"
 fi
