392b6011ba021b76e659a57b536fc49621d3fb64
S0ly <matis.mounic@epitech.eu>
2024-06-09 18:38:50 +0200

    refactor: added warning and deleted useless check in installer first step

--- public/install/functions.php
+++ public/install/functions.php
@@ -49,36 +49,6 @@ function checkWriteable(): bool
 }
 
 /**
- * Check if the server runs using HTTPS
- * @return bool Returns true on HTTPS or false on HTTP.
- */
-function checkHTTPS(): bool
-{
-    $isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443;
-    wh_log('https:', 'debug', (array)$isHttps);
-    return $isHttps;
-}
-
-/**
- * Check if MySQL is installed and runs the correct version using a shell command
- * @return mixed|string 'OK' if required version is met, returns MySQL version if not met.
- */
-function getMySQLVersion(): mixed
-{
-    global $requirements;
-
-    wh_log('attempting to get mysql version', 'debug');
-
-    $output = shell_exec('mysql -V') ?? '';
-    preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
-
-    $versionoutput = $version[0] ?? '0';
-    wh_log('mysql version: ' . $versionoutput, 'debug');
-
-    return intval($versionoutput) > intval($requirements['mysql']) ? 'OK' : $versionoutput;
-}
-
-/**
  * Check if zip is installed using a shell command
  * @return string 'OK' on success and 'not OK' on failure.
  */
--- public/install/index.php
+++ public/install/index.php
@@ -72,16 +72,10 @@ function cardStart($title, $subtitle = null)
 
         <ul class="list-none mb-2">
 
-            <li class="<?php echo checkHTTPS() == true ? 'ok' : 'not-ok'; ?> check">HTTPS is required</li>
-
             <li class="<?php echo checkWriteable() == true ? 'ok' : 'not-ok'; ?> check">Write-permissions on .env-file</li>
 
             <li class="<?php echo checkPhpVersion() === 'OK' ? 'ok' : 'not-ok'; ?> check">
                 php version: <?php echo phpversion(); ?> (minimum required <?php echo $requirements['minPhp']; ?>)
-            </li>
-
-            <li class="<?php echo getMySQLVersion() === 'OK' ? 'ok' : 'not-ok'; ?> check">
-                mysql version: <?php echo getMySQLVersion(); ?> (minimum required <?php echo $requirements['mysql']; ?>)
             </li>
 
             <li class="<?php echo count(checkExtensions()) == 0 ? 'ok' : 'not-ok'; ?> check">
@@ -93,10 +87,6 @@ function cardStart($title, $subtitle = null)
                 echo count(checkExtensions()) == 0 ? '' : '(Proceed anyway)'; ?>
             </li>
 
-
-            <!-- <li class="<?php echo getZipVersion() === 'OK' ? 'ok' : 'not-ok'; ?> check"> Zip
-                    version: <?php echo getZipVersion(); ?> </li> -->
-
             <li class="<?php echo getGitVersion() === 'OK' ? 'ok' : 'not-ok'; ?> check">
                 Git version:
                 <?php echo getGitVersion(); ?>
@@ -106,6 +96,16 @@ function cardStart($title, $subtitle = null)
                 Tar version:
                 <?php echo getTarVersion(); ?>
             </li>
+
+            <li>
+                <p class="text-neutral-400 mb-1">
+                    <br>
+                    <span style="color: #eab308;">Important:</span>
+                    CtrlPanel.gg requires a MySQL-Database, Redis-Server, and Pterodactyl-Panel to work.<br>
+                    Please make sure you have these installed and running before you continue.
+                </p>
+            </li>
+
         </ul>
 
         </div>
