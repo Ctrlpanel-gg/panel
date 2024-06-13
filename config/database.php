b9a1cd8e52054ac5286b44dcfc8ab8a272083e99
S0ly <matis.mounic@epitech.eu>
2024-06-13 11:17:55 +0200

    fix: changed database oops reverting back change

--- config/database.php
+++ config/database.php
@@ -119,7 +119,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
