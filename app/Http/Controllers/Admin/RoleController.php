df35503b177227317626bc87526d8fc2f9f3ce1e
Zastinian <zastinian@gmail.com>
2024-07-25 10:09:14 -0600

    fix: Store & Update role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -72,7 +72,8 @@ class RoleController extends Controller
         ]);
 
         if ($request->permissions) {
-            $role->givePermissionTo($request->permissions);
+            $collectedPermissions = collect($request->permissions)->map(fn($val)=>(int)$val);
+            $role->givePermissionTo($collectedPermissions);
         }
 
         return redirect()
@@ -123,7 +124,8 @@ class RoleController extends Controller
 
         if ($request->permissions) {
             if($role->id != 1){ //disable admin permissions change
-                $role->syncPermissions($request->permissions);
+                $collectedPermissions = collect($request->permissions)->map(fn($val)=>(int)$val);
+                $role->syncPermissions($collectedPermissions);
             }
         }
 
