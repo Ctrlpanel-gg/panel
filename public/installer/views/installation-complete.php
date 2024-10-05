2a9866e460bf18ae990e63401192971b9a4ba90c
Zastinian <zastinian@gmail.com>
2024-10-05 15:04:07 -0600

    fix: Early creation of env file

--- public/installer/views/installation-complete.php
+++ public/installer/views/installation-complete.php
@@ -1,22 +1,27 @@
-
 <!-- top layout here -->
 
 <?php
-$lockfile = fopen('../../install.lock', 'w') or exit('Unable to open file!');
-fwrite($lockfile, 'the installation is locked, delete this file to unlock it');
-fclose($lockfile);
+// Check if the installation is already locked
+if (!file_exists('../../install.lock')) {
+    echo cardStart(
+        $title = "Installation Complete!",
+        $subtitle = "You may navigate to your Dashboard now and log in!"
+    );
+    ?>
 
-echo cardStart(
-    $title = "Installation Complete!",
-    $subtitle = "You may navigate to your Dashboard now and log in!"
-);
+    <a href="<?php echo getenv('APP_URL'); ?>" class="w-full flex justify-center">
+        <button
+            class="mt-2 px-4 py-2 font-bold rounded-md bg-green-500/90 hover:bg-green-600 shadow-green-400 focus:outline-2 focus:outline focus:outline-offset-2 focus:outline-green-500">
+            Let's Go!
+        </button>
+    </a>
+
+    <?php
+    // Create the lock file after displaying the completion message
+    $lockfile = fopen('../../install.lock', 'w') or exit('Unable to open file!');
+    fwrite($lockfile, 'the installation is locked, delete this file to unlock it');
+    fclose($lockfile);
+}
 ?>
-
-<a href="<?php echo getenv('APP_URL'); ?>" class="w-full flex justify-center">
-    <button
-        class="mt-2 px-4 py-2 font-bold rounded-md bg-green-500/90 hover:bg-green-600 shadow-green-400 focus:outline-2 focus:outline focus:outline-offset-2 focus:outline-green-500">
-        Lets Go!
-    </button>
-</a>
 
 <!-- bottom layout here -->
