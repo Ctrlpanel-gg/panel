0c78f1768e905c357ab368725636e10186d19467
1day2die <ownerdennis8@gmail.com>
2024-11-11 11:47:51 +0100

    Update DiscordServiceProvider.php

--- app/Providers/DiscordServiceProvider.php
+++ app/Providers/DiscordServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use App\Settings\DiscordSettings;
 
