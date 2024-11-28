404959950d04ca14179561c8e1958837b53903e6
1day2die <ownerdennis8@gmail.com>
2024-11-28 10:15:52 +0100

    Better activity log

--- app/Models/User.php
+++ app/Models/User.php
@@ -327,8 +327,8 @@ class User extends Authenticatable implements MustVerifyEmail
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
-            ->logOnly(['role', 'name', 'server_limit', 'pterodactyl_id', 'email'])
-            ->logOnlyDirty()
-            ->dontSubmitEmptyLogs();
+            ->logOnly(['role', 'name', 'server_limit', 'pterodactyl_id', 'email', 'credits', 'server_limit', 'suspended', 'referral_code'])
+            ->logOnlyDirty();
     }
+
 }
