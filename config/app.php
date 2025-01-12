7e9ccdf7dba5e4b5e37dcea0c214b555232d5be0
Dennis <ownerdennis8@gmail.com>
2025-01-12 01:32:11 +0100

    Update Versionnumber

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'version' => '1.0.3',
+    'version' => '1.0.4',
 
     /*
     |--------------------------------------------------------------------------
