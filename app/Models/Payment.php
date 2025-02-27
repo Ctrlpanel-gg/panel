fba24bff90c75fc251711cb30916ed3b2885b8b2
LakshmiBhaskarPVL <16p3034@kennedyglobalschool.edu.in>
2025-02-27 21:08:19 +0530

    1st Commit For Floating Point Issues

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\PaymentStatus;
+use App\Traits\HandlesMoneyFields;
 use Hidehalo\Nanoid\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +12,7 @@ use NumberFormatter;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, HandlesMoneyFields;
 
     public $incrementing = false;
     protected $primaryKey = 'id';
@@ -39,7 +40,10 @@ class Payment extends Model
      * @var string[]
      */
     protected $casts = [
-        'status' => PaymentStatus::class
+        'status' => PaymentStatus::class,
+        'price' => 'integer',
+        'tax_value' => 'integer',
+        'total_price' => 'integer'
     ];
 
     public static function boot()
@@ -71,5 +75,65 @@ class Payment extends Model
         $formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
 
         return $formatter->formatCurrency($value, $this->currency_code);
+    }
+
+    /**
+     * @description Get the Formatted price attribute.
+     *
+     * @return float
+     */
+    public function getPriceAttribute($value)
+    {
+        return $this->convertFromInteger($value);
+    }
+
+    /**
+     * @description Set the Formatted price attribute.
+     *
+     * @return int 
+     */
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = $this->convertToInteger($value);
+    }
+
+    /**
+     * @description Tax value accessor
+     * 
+     * @return float
+     */
+    public function getTaxValueAttribute($value)
+    {
+        return $this->convertFromInteger($value);
+    }
+
+    /**
+     * @description Tax value mutator
+     * 
+     * @return int
+     */
+    public function setTaxValueAttribute($value)
+    {
+        $this->attributes['tax_value'] = $this->convertToInteger($value);
+    }
+
+    /**
+     * @description Total price accessor
+     * 
+     * @return float
+     */
+    public function getTotalPriceAttribute($value)
+    {
+        return $this->convertFromInteger($value);
+    }
+
+    /**
+     * @description Total price mutator
+     * 
+     * @return int
+     */
+    public function setTotalPriceAttribute($value)
+    {
+        $this->attributes['total_price'] = $this->convertToInteger($value);
     }
 }
