a4f0d9369cd588ea2de205ae1566b2d3314408d4
LakshmiBhaskarPVL <16p3034@kennedyglobalschool.edu.in>
2025-06-13 17:44:48 +0530

    More Gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     "./themes/default/css/**/*.css",
     "./themes/default/new-css/**/*.css",
   ],
+
   theme: {
     extend: {
       colors: {
@@ -33,18 +34,100 @@ module.exports = {
         surface: {
           DEFAULT: 'rgb(24 24 27 / 0.5)',
           border: 'rgb(39 39 42 / 0.5)',
-        }
+        },
+        p1: '#2EF2FF',
+        p2: '#3C52D9',
+        p3: '#C8EA80',
+        p4: '#EAEDFF',
+        p5: '#C4CBF5',
+        s1: '#080D27',
+        s2: '#0C1838',
+        s3: '#334679',
+        s4: '#1959AD',
+        s5: '#263466',
+        black: {
+          DEFAULT: '#000000',
+          100: '#05091D',
+        },
       },
+
+
+
+      boxShadow: {
+        100: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 16px 24px rgba(0, 0, 0, 0.25), inset 0px 3px 6px #1959AD',
+        200: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 16px 24px rgba(0, 0, 0, 0.25), inset 0px 4px 10px #3391FF',
+        300: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 16px 24px rgba(0, 0, 0, 0.25), inset 0px 3px 6px #1959AD',
+        400: 'inset 0px 2px 4px 0 rgba(255, 255, 255, 0.05)',
+        500: '0px 16px 24px rgba(0, 0, 0, 0.25), 0px -14px 48px rgba(40, 51, 111, 0.7)',
+      },
+
+      transitionProperty: {
+        borderColor: 'border-color',
+      },
+
+      spacing: {
+        '1/5': '20%',
+        '2/5': '40%',
+        '3/5': '60%',
+        '4/5': '80%',
+        '3/20': '15%',
+        '7/20': '35%',
+        '9/20': '45%',
+        '11/20': '55%',
+        '13/20': '65%',
+        '15/20': '75%',
+        '17/20': '85%',
+        '19/20': '95%',
+        22: '88px',
+        100: '100px',
+        330: '330px',
+        388: '388px',
+        400: '400px',
+        440: '440px',
+        512: '512px',
+        640: '640px',
+        960: '960px',
+        1230: '1230px',
+      },
+
+      zIndex: {
+        1: '1',
+        2: '2',
+        4: '4',
+      },
+
+      lineHeight: {
+        12: '48px',
+      },
+
+      borderRadius: {
+        14: '14px',
+        20: '20px',
+        40: '40px',
+        half: '50%',
+        '7xl': '40px',
+      },
+
+      flex: {
+        50: '0 0 50%',
+        100: '0 0 100%',
+        280: '0 0 280px',
+        256: '0 0 256px',
+        300: '0 0 300px',
+        320: '1px 0 320px',
+        540: '0 0 540px',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      }
+      },
     },
   },
+
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('flowbite'),
     require('flowbite-typography'),
-
   ],
-}
+};
