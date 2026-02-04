7f0ffc8396cb20b0306ffab961ec204beca0b86c
LakshmiBhaskarPVL <16p3034@kennedyglobalschool.edu.in>
2026-02-04 17:35:51 +0530

    fix: required variables swal2 modal having random html in it

--- themes/default/views/servers/create.blade.php
+++ themes/default/views/servers/create.blade.php
@@ -593,10 +593,8 @@
                                     `;
                                     })()
                                     : `<input id="${variable.env_variable}" name="${variable.env_variable}" type="text" required="required" class="form-control">`
-                        } <
-                        div id = "${variable.env_variable}-error"
-                        class = "mt-1" > < /div> <
-                        /div>
+                        } <div id = "${variable.env_variable}-error"
+                        class = "mt-1" > </div> </div>
                         `).join('')
                             }
                         `,
