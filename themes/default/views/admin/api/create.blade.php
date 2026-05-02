d4044139dff10673f92a6a870db30ce8c95aeb96
Zastinian <zastinian@gmail.com>
2026-05-02 15:38:50 -0600

    feat: Update Api Create View With Permissions Expiration And Token Display

--- themes/default/views/admin/api/create.blade.php
+++ themes/default/views/admin/api/create.blade.php
@@ -45,6 +45,39 @@
                                     @enderror
                                 </div>
 
+                                <div class="form-group">
+                                    <label for="expires_at">{{__('Expires At (Optional)')}}</label>
+                                    <input value="{{old('expires_at')}}" id="expires_at" name="expires_at" type="datetime-local"
+                                           class="form-control @error('expires_at') is-invalid @enderror">
+                                    @error('expires_at')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <label>{{__('Permissions (Optional - leave empty for full access)')}}</label>
+                                    <div class="row">
+                                        @foreach(\Spatie\Permission\Models\Permission::all() as $permission)
+                                            <div class="col-md-6">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" name="permissions[]"
+                                                           value="{{$permission->name}}" id="perm_{{$permission->id}}">
+                                                    <label class="form-check-label" for="perm_{{$permission->id}}">
+                                                        {{$permission->readable_name ?? $permission->name}}
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                    @error('permissions')
+                                    <div class="invalid-feedback d-block">
+                                        {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+
                                 <div class="form-group text-right">
                                     <button type="submit" class="btn btn-primary">
                                         {{__('Submit')}}
@@ -53,6 +86,14 @@
 
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             </form>
+
+                            @if(session('new_token'))
+                                <div class="alert alert-success mt-3">
+                                    <h5>{{__('API Token Created!')}}</h5>
+                                    <p>{{__('Copy this token now. It will not be shown again!')}}</p>
+                                    <code class="d-block p-2 bg-light">{{session('new_token')}}</code>
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -61,7 +102,6 @@
         </div>
     </section>
     <!-- END CONTENT -->
-
 
 
 @endsection
