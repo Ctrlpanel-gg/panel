39c95cfaf9103cbc0e3d19954beaf033162347b9
simbabimba-dev <79574809+simbabimba-dev@users.noreply.github.com>
2026-05-06 17:01:00 +0000

    remove discordUser.id filter in usercontroller's json method

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -151,7 +151,7 @@ class UserController extends Controller
         $this->checkPermission(self::READ_PERMISSION);
 
         $users = QueryBuilder::for(User::query())
-            ->allowedFilters(['id', 'name', 'pterodactyl_id', 'email','discordUser.id'])
+            ->allowedFilters(['id', 'name', 'pterodactyl_id', 'email'])
             ->paginate(25);
 
         if ($request->query('user_id')) {
